134e0078f9e55e8330348f7095cfff8658a4fc46
DM Cyber Security <128435685+dmcys@users.noreply.github.com>
2023-05-19 17:36:16 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!bin/sh
-read -p "You want install it on ArchLinux? y/n" option;
-if $option == "y"; then
+read -p "You want install it on ArchLinux? y/n " option
+if [ $option == "y" ]; then
 #Arch Linux Build
   sudo pacman -S jdk8-openjdk
   sudo pacman -S ant
@@ -10,3 +10,4 @@ if $option == "y"; then
 else
   echo "Exiting..";
 fi
+
